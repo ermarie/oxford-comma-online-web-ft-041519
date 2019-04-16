82fe757f4551b7369fffc3530447d0994f18bb22
Erin Stalzer <erinstalzer@gmail.com>
2019-04-16 01:28:26 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,14 +3,12 @@ def oxford_comma(array)
     array[0]
   elsif array.length == 2
     array.join(" and ")
-  elsif array.length == 3
+  else
     end_element = array[array.length - 1]
     array.pop
     string = array.join(", ")
     string << ", and "
     string << end_element
     string
-  else
-
   end
 end
